0d29b1f94ae13894935f57c2ef6b435670e38dd6
Ian <itcropper@gmail.com>
2015-04-05 08:59:02 -0600

    added region

--- models/horn.js
+++ models/horn.js
@@ -20,7 +20,8 @@ app.use(busboy());
 app.use(bodyParser.json());
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'us-west-1'
 });
 
 
