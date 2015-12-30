0cf9d9dcb79f487f2232c9685bdb3bdf977b9d77
Ian <itcropper@gmail.com>
2015-12-30 13:11:47 -0700

    testing keys

--- keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = function keys() {
-  return {
-    name    : "Apollo",
-    key     : "AKIAI246JNH62KHUTHOA",
-    secret  : "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"
-  };
-}
--- models/pocket.js
+++ models/pocket.js
@@ -3,23 +3,24 @@ var mongoose = require('mongoose'),
 
 var PocketSchema = new Schema({
     name: String,
-    location:{
+    location    : {
         type: { type: String }, 
         coordinates: []
     },
-    address: {
+    address:     {
         street: String,
         city: String,
         state: String,
         zip: String
     },
-    author_id: { type: String, index: true },
-    created: Date,
-    events: [String] 
+    author_id   : { type: String, index: true },
+    created     : Date,
+    events      : [String],
+    name        : String,
+    description : String,
+    radius      Number
 });
 
 PocketSchema.index({ location : '2dsphere' });
-
-
 
 module.exports = mongoose.model('Pocket', PocketSchema);
